e01832fc2b204e458419d54c6226393914360c6c
parag07jadhav <parag07.jadhav@gmail.com>
2025-04-30 18:31:21 +0530

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type
